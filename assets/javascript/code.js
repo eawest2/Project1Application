afd115060043d8e5cd0b56e3d0a73ef69a80ae99
Tyrel Hooker <tyrelhooker@gmail.com>
2018-04-04 21:25:58 -0400

    add ajax query for open weather API and added jQuery selectors to FXwriteFacts ()

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -6,7 +6,7 @@
 
 var localLocation = 0;
 var marsWeather = 0;
-var lolcalWeather = 0;
+var localWeather = 0;
 var marsPhoto = 0;
 
 
@@ -15,7 +15,29 @@ var marsPhoto = 0;
 
     //Geolocation ** STRETCH GOAL**
 
-    //Weather Call function
+    //Open Weather Call function
+    // This is our API key
+    var APIKey = "7d2ff8f5647ce6dbd5231ca3f107d20b";
+    var city = "";
+    var country = "";
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+      "q=" + city + country + "&units=imperial&appid=" + APIKey;
+      
+    // Here we run our AJAX call to the OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+      .then(function(response) {
+        // Log the queryURL
+        console.log(queryURL);
+        // Log the resulting object
+        console.log(response);
+        // Assigns the response object to global variable
+        localWeather = response; 
 
     //Mars call function
 
@@ -50,6 +72,9 @@ var marsPhoto = 0;
     function FXwriteFacts (){
 
         //write weather info
+        $(".temp").text("Temperature (F) " + response.main.temp);
+        $(".weather").text(response.weather.description);
+
         //write mars info
         //write mars photo
 
