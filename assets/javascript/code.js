2b6279059fd9c9334ab947d8f20fd57fc16e0d71
unknown <eawest2@gmail.com>
2018-04-04 20:24:39 -0400

    added basic JS

---
+++ assets/javascript/code.js
@@ -0,0 +1,56 @@
+//Red Rover Code Base
+
+
+
+//Primary Variable Storage
+
+
+
+//AJAX call functions
+
+    //Geolocation ** STRETCH GOAL**
+
+    //Weather Call function
+
+    //Mars call function
+
+    //Mars photo call function
+
+
+//Function Declaration
+
+    //Function to initialize all page content
+    function FXstart (){
+
+    };
+
+    //Determine weather location
+        //Write weather options to DOM
+        function FXweatherPrint (){
+
+        };
+
+        //Geolocate user endpoint
+        function FXweatherGeolocation (){
+
+        };
+
+    //Write collected info to DOM
+    function FXwriteFacts (){
+
+        //write weather info
+        //write mars info
+        //write mars photo
+
+    };
+
+    //Compute delta
+        //compute deltaTemp
+        //compute deltaWeather
+        //compute deltaSeason
+
+
+
+//Initialize
+
+FXstart();
