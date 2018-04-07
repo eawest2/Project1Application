d257eb897b6ae1b8804a34614b426781767ced7c
npasher <n.p.asher@gmail.com>
2018-04-07 09:53:55 -0400

    howler

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,5 +1,5 @@
 //Red Rover Code Base
-
+$( document ).ready(function() {
 
 
 //Primary Variable Storage
@@ -120,3 +120,43 @@ var marsPhoto = 0;
 
 FXstart();
 displayMarsWeather();
+//howler player function//
+$(function(){
+
+	var howler_example = new Howl({
+		src: ["assets/audio/8BallAitken_CycloneCountry.mp3"],
+		volume: 0.5
+	});
+
+	$("#howler-play").on("click", function(){
+		howler_example.play();
+	});
+
+	$("#howler-pause").on("click", function(){
+		howler_example.pause();
+	});
+
+	$("#howler-stop").on("click", function(){
+		howler_example.stop();
+	});
+
+	$("#howler-volup").on("click", function(){
+		var vol = howler_example.volume();
+		vol += 0.1;
+		if (vol > 1) {
+			vol = 1;
+		}
+		howler_example.volume(vol);
+	});
+
+	$("#howler-voldown").on("click", function(){
+		var vol = howler_example.volume();
+		vol -= 0.1;
+		if (vol < 0) {
+			vol = 0;
+		}
+		howler_example.volume(vol);
+	});
+
+});
+});
