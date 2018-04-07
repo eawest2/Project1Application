46139f7930d1232aa81c025a9d5c8321b2dae473
unknown <eawest2@gmail.com>
2018-04-07 12:02:59 -0400

    pushing geolocation code

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -121,7 +121,32 @@ var marsPhoto = 0;
 
         //Geolocate user endpoint
         function FXweatherGeolocation (){
-
+                var options = {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0
+                };
+                
+                function success(pos) {
+                    var crd = pos.coords;
+                
+                    console.log('Your current position is:');
+                    console.log(`Latitude : ${crd.latitude}`);
+                    console.log(`Longitude: ${crd.longitude}`);
+    
+                    localLocation = {
+                    latitude: crd.latitude,
+                    longitude: crd.longitude
+                };
+                ;
+                }
+                
+                function error(err) {
+                    console.warn(`ERROR(${err.code}): ${err.message}`);
+                }
+                
+                navigator.geolocation.getCurrentPosition(success, error, options);
+        
         };
 
     //Write collected info to DOM
